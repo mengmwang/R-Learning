a95cf8aad4bdf40a603f229ac63fe5676cc13fa3
Mengmeng Wang <108039005+mengmwang@users.noreply.github.com>
2022-08-31 13:26:46 +1000

    Create practice.R

---
+++ practice.R
@@ -0,0 +1,21 @@
+# This is my first R script 
+# Some basic lines, for practice purposes
+
+a <- 5
+b <- 5
+a+b
+
+name <- "John"
+age <- 40
+
+name   # output "John"
+age    # output 40
+
+text <- "good"
+paste("ABC", text)
+
+a1 <- a2 <- a3 <- "Apple"
+
+a1
+a2
+a3
