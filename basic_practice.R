8227376551b56fd9a12410530e26c0ab36b3361b
Mengmeng Wang <108039005+mengmwang@users.noreply.github.com>
2022-08-31 13:41:42 +1000

    Update and rename practice.R to basic_practice.R

--- basic_practice.R
+++ basic_practice.R
@@ -19,3 +19,22 @@ a1 <- a2 <- a3 <- "Apple"
 a1
 a2
 a3
+
+num2 <- 3.8
+ii <- 5L
+co <- 1+2i
+char <- 'KKT'
+l <- TRUE
+
+ii
+class(ii)
+class(num2)
+
+num3 <- as.numeric(ii)
+num3
+class(num3)
+
+num4 <- as.complex(ii)
+num4
+class(num4)
+
