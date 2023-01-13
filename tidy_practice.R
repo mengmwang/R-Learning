50e9dc61db1c945a1ce9bfd92f8d992073a20e23
Mengmeng Wang <108039005+mengmwang@users.noreply.github.com>
2023-01-13 15:35:43 +1100

    Create tidy_practice.R

---
+++ tidy_practice.R
@@ -0,0 +1,13 @@
+install.packages("tidyverse")
+library(tidyverse)
+data()
+View(starwars)
+
+starwars %>% 
+  select(gender, mass, height, species) %>% 
+  filter(species == "Human") %>% 
+  na.omit() %>% 
+  mutate(height = height/100) %>% 
+  mutate(BMI = mass/height^2) %>% 
+  group_by(gender) %>% 
+  summarise(Average_BMI = mean(BMI))
