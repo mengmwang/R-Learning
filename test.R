50f7deb2eb9940a29fa7bf4be90d2aa4df382e85
Mengmeng Wang <108039005+mengmwang@users.noreply.github.com>
2023-02-27 17:29:03 +1100

    Update test.R

--- test.R
+++ test.R
@@ -3,3 +3,24 @@
 library(pacman)
 p_load("dplyr","tidyr","Hmisc")
 
+# load different data types
+# .sav file (for SPSS)
+library(haven)
+data1 <- read_sav("data1.sav")
+
+# load excel file
+library(readxl)
+data2 <- read_excel("data2.xlsx")
+
+# column names 
+names(data1)
+names(data2)
+
+# view data
+view(data1)
+view(data2)
+
+# compare the column names for two dataframes 
+# use compare() from waldo library
+library(waldo)
+compare(names(data1), names(data2))
